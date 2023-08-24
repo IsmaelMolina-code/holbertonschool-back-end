f9f50b4fb5b2a7fe2cbdff63c3d4ba5af2187d3d
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-08-24 00:11:08 -0300

    Task 0 of API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+import json
+from sys import argv
+
+if __name__ == '__main__':
+
+    if len(argv) != 2:
+        exit()
+
+    users_request = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    users = json.loads(users_request.text)
+    all_users_id = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    all_users_id = json.loads(all_users_id.text)
+
+    tasks_completed = 0
+    tasks_not_completed = 0
+
+    for task in all_users_id:
+        if task['completed'] is True:
+            tasks_completed += 1
+        else:
+            tasks_not_completed += 1
+
+    print(f"Employee {users['name']} is done with tasks"
+          f"({tasks_completed}/{len(all_users_id)}):")
+
+    for task in all_users_id:
+        if task['completed'] is True:
+            print("\t {}".format(task['title']))
---
+++ api/README.md
@@ -0,0 +1,1 @@
+#Folder containing the API excercises
